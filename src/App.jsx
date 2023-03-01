29fab97a26afaecdc64c6901df8abefabd27de46
Sarena Yang <sarenay2@illinois.edu>
2023-03-01 13:48:38 -0600

    added log in link to spotify

--- src/App.jsx
+++ src/App.jsx
@@ -4,29 +4,15 @@ import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
-
+  const CLIENT_ID = "7ae719c349a348cfbf6126bcd6125859"
+  const REDIRECT_URI = "http://localhost:5173/"
+  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
+  const RESPONSE_TYPE = "token"
   return (
     <div className="App">
       <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
   )
 }
