4c67b84020f352802f8194e9beb7a02d96fa6185
Sarena Yang <sarenay2@illinois.edu>
2023-03-01 23:30:28 -0600

    adding server side code

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,30 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import React, { useState, useEffect } from 'react';
+import WebPlayback from './components/WebPlayback'
+import Login from './components/Login'
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-  const CLIENT_ID = "7ae719c349a348cfbf6126bcd6125859"
-  const REDIRECT_URI = "http://localhost:5173/"
-  const AUTH_ENDPOINT = "https://accounts.spotify.com/authorize"
-  const RESPONSE_TYPE = "token"
+
+  const [token, setToken] = useState('');
+
+  useEffect(() => {
+
+    async function getToken() {
+      const response = await fetch('/auth/token');
+      const json = await response.json();
+      setToken(json.access_token);
+    }
+
+    getToken();
+
+  }, []);
+
   return (
-    <div className="App">
-      <div>
-      <a href={`${AUTH_ENDPOINT}?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=${RESPONSE_TYPE}`}>Login to Spotify</a>
-      </div>
-    </div>
-  )
+    <>
+        { (token === '') ? <Login/> : <WebPlayback token={token} /> }
+    </>
+  );
 }
 
-export default App
+
+export default App;
